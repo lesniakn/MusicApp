508c9d9c6728edccef9428b0dc583748f74e4f19
W57466 <norber.lesniak@gmail.com>
2018-06-23 10:23:23 +0200

    Pobieranie danych szczegółowych - poprawienie

--- app/src/main/java/com/example/stud/musicapp/topsongs/SongDetailsActivity.java
+++ app/src/main/java/com/example/stud/musicapp/topsongs/SongDetailsActivity.java
@@ -1,15 +1,19 @@
 package com.example.stud.musicapp.topsongs;
 
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.stud.musicapp.R;
 import com.example.stud.musicapp.api.ApiService;
+import com.example.stud.musicapp.api.Track;
 import com.example.stud.musicapp.api.Tracks;
 
+import retrofit2.Call;
 import retrofit2.Callback;
+import retrofit2.Response;
 
 public class SongDetailsActivity extends AppCompatActivity {
     public static final String TRACK = "track";
@@ -34,6 +38,36 @@ public class SongDetailsActivity extends AppCompatActivity {
         getSupportActionBar().setTitle(track);
         getSupportActionBar().setSubtitle(artist);
 
+        ApiService.getService().getTrack(trackId).enqueue(new Callback<Tracks>() {
+            @Override
+            public void onResponse(Call<Tracks> call, Response<Tracks> response) {
+                Tracks tracks = response.body();
+
+                if (tracks != null && tracks.track.size() > 0) {
+                    showData(tracks.track.get(0));
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Tracks> call, Throwable t) {
+                Toast.makeText(
+                        SongDetailsActivity.this,
+                        "Błąd pobierania danych: " + t.getLocalizedMessage(),
+                        Toast.LENGTH_SHORT
+                ).show();
+            }
+        });
+    }
+
+    private void showData(Track track) {
+        TextView tvAlbum = findViewById(R.id.tvAlbum);
+        TextView tvGenre = findViewById(R.id.tvGenre);
+        TextView tvStyle = findViewById(R.id.tvStyle);
+        TextView tvDescription = findViewById(R.id.tvDescription);
+        tvAlbum.setText(track.strAlbum);
+        tvGenre.setText(track.strGenre);
+        tvStyle.setText(track.strStyle);
+        tvDescription.setText(track.strDescriptionEN);
     }
 
     @Override
@@ -43,22 +77,5 @@ public class SongDetailsActivity extends AppCompatActivity {
         return true;
     }
 
-    ApiService.getService().getTrack(trackId).enqueue(new Callback<Tracks>() {
-        @Override
-        public void onResponse(@NonNull Call<Tracks> call, @NonNull Response<Tracks>response) {
-            Toast. makeText (
-                    SongDetailsActivity. this ,
-                    "Pobrano dane" , Toast. LENGTH_SHORT
-            ).show();
-        }
 
-        @Override
-        public void onFailure(@NonNull Call<Tracks> call, @NonNull Throwable t) {
-            Toast. makeText (
-                    SongDetailsActivity. this ,
-                    "Błąd pobierania danych: " + t.getLocalizedMessage(),
-                    Toast. LENGTH_SHORT
-            ).show();
-        }
-    });
 }
